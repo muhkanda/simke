0d05dfca8186aa5bc99db370165cf75054f8106b
Mohamad Iqbal Musyaffa <m.iqbal@bigio.id>
2019-12-17 14:40:59 +0700

    Database set Online

--- functions/database.php
+++ functions/database.php
@@ -1,4 +1,4 @@
-<?php
+<!-- <?php
 	$host	= "localhost";
 	$uname	= "root";
 	$pass	= "";
@@ -7,4 +7,20 @@
 	if (mysqli_connect_errno()) {
 		echo "Koneksi Database Gagal : ".mysqli_connect_errno();
 	}
+?> -->
+
+<?php
+	$host	= "muhkanda.tech";
+	$uname	= "jwwunqak_simke";
+	$pass	= "@Adminer2019";
+	$db 	= "jwwunqak_simke";
+	$conn 	= mysqli_connect($host, $uname, $pass, $db);
+	if (mysqli_connect_errno()) {
+		echo "Koneksi Database Gagal : ".mysqli_connect_errno();
+	}
 ?>
+
+<!-- 
+	U : jwwunqak_simke
+	P : @Adminer2019
+ -->
