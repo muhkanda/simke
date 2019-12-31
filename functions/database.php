30fe9e472d322ff2d0840b0366e73ad7b1f8c79c
Muh Kanda <melodic12@windowslive.com>
2019-12-31 21:56:02 +0700

    :fire: Removing Old Databases
    Removing Old Databases

--- functions/database.php
+++ functions/database.php
@@ -1,30 +1,3 @@
-<!-- <?php
-	$host	= "localhost";
-	$uname	= "root";
-	$pass	= "";
-	$db 	= "db_simke";
-	$conn 	= mysqli_connect($host, $uname, $pass, $db);
-	if (mysqli_connect_errno()) {
-		echo "Koneksi Database Gagal : ".mysqli_connect_errno();
-	}
-?> -->
-
-<!-- <?php
-	$host	= "muhkanda.tech";
-	$uname	= "jwwunqak_simke";
-	$pass	= "@Adminer2019";
-	$db 	= "jwwunqak_simke";
-	$conn 	= mysqli_connect($host, $uname, $pass, $db);
-	if (mysqli_connect_errno()) {
-		echo "Koneksi Database Gagal : ".mysqli_connect_errno();
-	}
-?> -->
-
-<!-- 
-	U : jwwunqak_simke
-	P : @Adminer2019
- -->
-
 <?php
 	$host	= "remotemysql.com";
 	$uname	= "bSghyCT7Rh";
@@ -35,8 +8,15 @@
 		echo "Koneksi Database Gagal : ".mysqli_connect_errno();
 	}
 ?>
-
 <!-- 
+	~~~~~~~~~~~~~~~~~
+	H : muhkanda.tech
+	U : jwwunqak_simke
+	P : @Adminer2019
+	~~~~~~~~~~~~~~~~~
+	H : remotemysql.com
 	U : bSghyCT7Rh
 	P : N4iw7pNeLr
+	~~~~~~~~~~~~~~~~~
+	/ ~
  -->
